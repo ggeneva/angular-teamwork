513f4323289ccd014c74214dd84c45504903d792
Xpload <xpload@email.bg>
2017-08-23 19:34:58 +0300

    fixed user.module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,20 +5,16 @@ import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ProfileComponent } from './profile/profile.component';
-<<<<<<< HEAD
-import { UserRoutingModule } from "./user-routing.module";
-import { MdInputModule, MaterialModule } from "@angular/material";
-=======
+
+import { MdInputModule, MaterialModule } from '@angular/material';
 import { UserRoutingModule } from './user-routing.module';
->>>>>>> a417fe5ab58b3df2c1436b0a827b2bb47b9096a4
 
 @NgModule({
   imports: [
     UserRoutingModule,
     CommonModule,
     MaterialModule,
-    MdInputModule,
-    
+    MdInputModule
   ],
   declarations: [
     LoginComponent,
