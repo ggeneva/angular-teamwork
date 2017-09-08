0bfc49d2961c4beb8aa341c0ff584020e53e2102
Xpload <xpload@email.bg>
2017-09-08 16:52:39 +0300

    fixed merge conflicts and CircleCI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../providers/data.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public items: FirebaseListObservable<any>;
+  public shits: any;
+
+  constructor(public dataService: DataService) {
+    this.items = dataService.test.getObservableList();
+
+    // this.dataService.test.update('-KsiZGMi-PQdqU5sqJKG', { testfieldTwo: 'waw!!!' });
+  }
 
   ngOnInit() {
   }
---
+++ src/app/providers/data/base/base.data.ts
@@ -0,0 +1,68 @@
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+export class BaseData {
+    private list: FirebaseListObservable<any[]>;
+    private listName: string;
+
+    constructor(private db: AngularFireDatabase, private ModelClass) {
+        this.listName = this.getCollectionName();
+        this.list = this.db.list('/' + this.listName);
+    }
+
+    public add(model: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.list.push(model)
+                .catch((error) => {
+                    reject(error);
+                })
+                .then(() => {
+                    resolve();
+                });
+        });
+    }
+
+    public remove(key: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.list.remove(key)
+                .catch((error) => {
+                    reject(error);
+                })
+                .then(() => {
+                    resolve();
+                });
+        });
+    }
+
+    public update(key: string, data: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.list.update(key, data)
+                .catch((error) => {
+                    reject(error);
+                })
+                .then(() => {
+                    resolve();
+                });
+        });
+    }
+
+    public set(key: string, data: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.list.set(key, data)
+                .catch((error) => {
+                    reject(error);
+                })
+                .then(() => {
+                    resolve();
+                });
+        });
+    }
+
+    public getObservableList() {
+        return this.list;
+    }
+
+
+    private getCollectionName(): string {
+        return this.ModelClass.name.toLowerCase() + 's';
+    }
+}
