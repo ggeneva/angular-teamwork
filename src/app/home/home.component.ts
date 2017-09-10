8f06d62758c0d894deae199d76afd989f40b15f3
Hristo Hristov <xpload@email.bg>
2017-09-10 20:36:16 +0300

    added last N recipes to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../providers/data.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor(public dataService: DataService) {
+  public lastRecipes;
+  private lastRecipesSub: ISubscription;
+
+  constructor(public db: DataService) {
 
     // this.dataService.test.update('-KsiZGMi-PQdqU5sqJKG', { testfieldTwo: 'waw!!!' });
   }
 
   ngOnInit() {
+    this.lastRecipesSub = this.db.recipes.getLastNRecipes(3)
+      .subscribe(data => {
+        console.log(data);
+        this.lastRecipes = data;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.lastRecipesSub.unsubscribe();
+  }
 }
