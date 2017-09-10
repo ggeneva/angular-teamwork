a1113f5757027e3b6cff312bb06181c8fc7fee1f
Exodary <krasimir_zlatkov@mail.bg>
2017-09-11 01:08:55 +0300

    Removing auto generated tests

--- src/app/user/logout/logout.component.spec.ts
+++ src/app/user/logout/logout.component.spec.ts
@@ -19,7 +19,7 @@ describe('LogoutComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
