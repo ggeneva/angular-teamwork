1fb65e20e98140e013631f523264968cde308818
Exodary <krasimir_zlatkov@mail.bg>
2017-09-11 01:09:43 +0300

    Adding unit test for ErrorComponent

--- src/app/error/error.component.spec.ts
+++ src/app/error/error.component.spec.ts
@@ -1,13 +1,18 @@
+import { element } from 'protractor';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { ErrorComponent } from './error.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
 
+  const currentPath = 'Random';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ ErrorComponent ]
     })
     .compileComponents();
@@ -16,10 +21,25 @@ describe('ErrorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorComponent);
     component = fixture.componentInstance;
+    component.currentPath = currentPath;
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should display current Path in paragraph element', () => {
+    // Arrange.
+    const de = fixture.debugElement.query(By.css('p'));
+    const el: HTMLElement = de.nativeElement;
+
+    // Assert.
+    expect(el.innerHTML).toContain(currentPath);
+  });
+
+  it('should display Page not found in break element', () => {
+    // Arrange.
+    const de = fixture.debugElement.query(By.css('p'));
+    const el: HTMLElement = de.nativeElement;
+
+    // Assert.
+    expect(el.innerHTML).toContain('Page not found!');
   });
 });
